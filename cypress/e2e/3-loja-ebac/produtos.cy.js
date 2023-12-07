b5dc2a9f8566f414f2ce086db570ebce2359d845
fabioaraujoqa <fabioaraujoqa@gmail.com>
2023-12-07 03:33:44 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,34 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Frankie Sweatshirt')
-            .click()
+        produtosPage.buscarProdutoLista('Beaumont Summit Kit')
             cy.get('#tab-title-description > a').should('contain', 'Descrição')        
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto )
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant' )
+    });
+
+    it.only('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Blue', qtd)
+
+        cy.get('.woocommerce-message').should('contain',  qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
     });
 });
